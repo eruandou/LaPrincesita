4f5cb13120029e480bef845343adda472de568fe
eruandou <zambra_nacho@hotmail.com>
2022-10-12 19:05:37 -0300

    Killzones

--- LaPrincesaTeresa/Assets/Scripts/KillZone.cs
+++ LaPrincesaTeresa/Assets/Scripts/KillZone.cs
@@ -1,7 +1,9 @@
 using System.Linq;
 using Interface;
 using UnityEngine;
+using UnityEngine.Scripting;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class KillZone : MonoBehaviour
 {
     private ILevelResetable[] _allResetables;
