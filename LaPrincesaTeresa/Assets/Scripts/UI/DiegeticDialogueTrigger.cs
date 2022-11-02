d26c7cef0008d7d742a8b34cb6c3abd4ecb58ff2
eruandou <zambra_nacho@hotmail.com>
2022-11-02 14:51:07 -0300

    Fix coroutine repeating problem

--- LaPrincesaTeresa/Assets/Scripts/UI/DiegeticDialogueTrigger.cs
+++ LaPrincesaTeresa/Assets/Scripts/UI/DiegeticDialogueTrigger.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using ScriptableObjects.Dialogue;
 using ScriptableObjects.Events;
@@ -12,25 +13,47 @@ public class DiegeticDialogueTrigger : MonoBehaviour
     [SerializeField] private LayerMask playerCollision;
     [SerializeField] private bool isRepeatable;
     [SerializeField] private float repeatCooldown = 3;
+    private Coroutine _eventCoroutine;
+    private WaitForSeconds _waitTime;
+
+
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (_triggered || !GameStaticFunctions.IsGoInLayerMask(col.gameObject, playerCollision)) return;
+        if (_triggered || !GameStaticFunctions.IsGoInLayerMask(col.gameObject, playerCollision) ||
+            _eventCoroutine != default) return;
+
+        _triggered = true;
 
         if (isRepeatable)
         {
-            StartCoroutine(RepeatCooldown());
+            _eventCoroutine = StartCoroutine(RepeatCooldown());
         }
-        else
-        {
-            _triggered = true;
-        }
+
         uiEvent.Raise(new UIParams(UICommand.DiegeticDialogueCommand, dialogue));
     }
 
-    IEnumerator RepeatCooldown()
+    private IEnumerator RepeatCooldown()
     {
-        _triggered = true;
-        yield return new WaitForSeconds(repeatCooldown);
+        _waitTime ??= new WaitForSeconds(repeatCooldown);
+        yield return _waitTime;
         _triggered = false;
+        _eventCoroutine = default;
     }
+
+#if UNITY_EDITOR
+    [ContextMenu("Get minimum wait time")]
+    private void GetWaitTime()
+    {
+        var proposedWaitTime = dialogue.GetAllDialogueDelay();
+        if (proposedWaitTime > repeatCooldown)
+        {
+            repeatCooldown = proposedWaitTime;
+        }
+    }
+
+    private void OnValidate()
+    {
+        GetWaitTime();
+    }
+#endif
 }
