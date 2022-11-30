45876e071a9d8bc628c002d7410eed8502c60429
Huergacho <75048402+Huergacho@users.noreply.github.com>
2022-11-30 12:02:14 -0300

    Tuto part 1

--- LaPrincesaTeresa/Assets/Scripts/InteractableObjects/FallingPlatform.cs
+++ LaPrincesaTeresa/Assets/Scripts/InteractableObjects/FallingPlatform.cs
@@ -1,24 +1,23 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class FallingPlatform : MonoBehaviour
 {
     [SerializeField] private float timer;
     [SerializeField] private float countDown;
     [SerializeField] private GameObject visual;
+    [SerializeField] private bool hasToFall = true;
+    [SerializeField]private bool restore = false;
 
     private Vector3 _origin;
     private float _currCd;
     private float _destroyTime;
     private Rigidbody2D _rb;
     private Collider2D[] _collider2Ds;
-    private bool _hasToFall;
-    private bool _restore;
     private bool _activeTrap;
     private Animator _animator;
     private void Awake()
     {
-        _hasToFall = true;
-        _restore = false;
         _activeTrap = false;
         
         _origin = transform.position;
@@ -34,26 +33,16 @@ public class FallingPlatform : MonoBehaviour
     {
         if (!_activeTrap) return;
 
-        if (_currCd < Time.time && _hasToFall)
+        if (_currCd < Time.time && hasToFall)
         {
             _rb.isKinematic = false;
-            _hasToFall = false;
+            hasToFall = false;
             _destroyTime = Time.time + countDown;
         }
 
-        if (!_hasToFall && _destroyTime < Time.time)
+        if (!hasToFall && _destroyTime < Time.time)
         {
             EnableDisableThingies(false);
-        }
-
-        if (_restore)
-        {
-            EnableDisableThingies(true);
-            ReproduceAnimation(false);
-            _hasToFall = true;
-            _activeTrap = false;
-            _rb.isKinematic = true;
-            _rb.velocity = Vector2.zero;
         }
     }
 
@@ -68,12 +57,21 @@ public class FallingPlatform : MonoBehaviour
         if (enaDis)
         {
             transform.position = _origin;
-            _restore = false;
         }
         else
         {
-            _restore = true;
+            Restore();
         }
+    }
+
+    void Restore()
+    {
+        EnableDisableThingies(true);
+        ReproduceAnimation(false);
+        hasToFall = true;
+        _activeTrap = false;
+        _rb.isKinematic = true;
+        _rb.velocity = Vector2.zero;
     }
 
 
---
+++ LaPrincesaTeresa/Assets/Scripts/TriggerSceneChanger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using Attributes;
+using DefaultNamespace;
+using UnityEditor;
+public class TriggerSceneChanger : MonoBehaviour
+{
+    [SerializeField] private LayerMask contactLayers;
+#if UNITY_EDITOR
+    [SerializeField] private SceneAsset nextLevelScene;
+#endif
+    [ReadOnlyInspector, SerializeField] private string nextLevelSceneName;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (GameStaticFunctions.IsGoInLayerMask(other.gameObject, contactLayers))
+        {
+         GameManager.Instance.CustomSceneManager.ChangeScene(nextLevelSceneName);
+        }
+    }
+#if UNITY_EDITOR
+    [ContextMenu("Get scene name")]
+    private void GetSceneName()
+    {
+        nextLevelSceneName = nextLevelScene.name;
+        EditorUtility.SetDirty(gameObject);
+    }
+
+#endif
+}
